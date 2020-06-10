775fdb727e50a591fdcf11b905b88b305d83ea82
Kushal Mukherjee <kushal.mukherjee28exm@gmail.com>
2020-06-11 01:05:36 +0530

    Build issue fix & more cleanup

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,4 @@
-import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import Head from "next/head";
 
 export default function Meta() {
   return (
@@ -7,19 +6,7 @@ export default function Meta() {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href="/favicon/favicon.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
@@ -28,15 +15,9 @@ export default function Meta() {
         color="#000000"
       />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta name="description" content="Welcome to our world." />
+      <meta property="og:image" content="/favicon.png" />
     </Head>
-  )
+  );
 }
