b5a155aaed50910b0960c49466e3f471aa25e42b
Kushal Mukherjee <kushal.mukherjee28exm@gmail.com>
2020-06-11 00:44:13 +0530

    Fresh Start

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Container from "../components/container";
-import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
 import Intro from "../components/intro";
 import Layout from "../components/layout";
 import { getAllPosts } from "../lib/api";
 import Head from "next/head";
 
-export default function Index({ allPosts }) {
+export default function Index() {
   return (
     <>
       <Layout>
@@ -15,25 +13,9 @@ export default function Index({ allPosts }) {
         </Head>
         <Container>
           <Intro />
-          <HeroPost />
-          {allPosts.length > 0 && <MoreStories posts={allPosts} />}
         </Container>
       </Layout>
     </>
   );
 }
 
-export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
-  ]);
-
-  return {
-    props: { allPosts },
-  };
-}
